9c1829e495995d425f024b09c1b309cb4bfa6414
YaelBusso <y0583239076@gmail.com>
2021-05-16 12:12:23 +0300

    client baseic

---
+++ client/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import TextEditor from './TextEditor'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom"
+import { v4 as uuidV4 } from "uuid"
+
+const App = () => {
+  return (
+    <div>
+      heree
+    </div>
+  )
+}
+
+export default App;
